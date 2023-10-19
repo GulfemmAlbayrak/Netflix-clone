5d36cab20a56af175ce01496fe4c31119dac8db2
Gülfem Albayrak <gulfemalbayrak@gmail.com>
2023-10-19 16:45:03 +0300

    fix some bugs

--- Netflix Clone/Views/CollectionViewTableViewCell.swift
+++ Netflix Clone/Views/CollectionViewTableViewCell.swift
@@ -53,6 +53,10 @@ class CollectionViewTableViewCell: UITableViewCell {
             self?.collectionView.reloadData()
         }
     }
+    
+    private func downloadTitleAt(indexPath: IndexPath){
+        print("Downloading \(titles[indexPath.row].original_title)")
+    }
 }
 
 extension CollectionViewTableViewCell: UICollectionViewDelegate, UICollectionViewDataSource {
@@ -98,12 +102,13 @@ extension CollectionViewTableViewCell: UICollectionViewDelegate, UICollectionVie
         }
     }
     //
-    func collectionView(_ collectionView: UICollectionView, contextMenuConfigurationForItemsAt indexPaths: [IndexPath], point: CGPoint) -> UIContextMenuConfiguration? {
+    func collectionView(_ collectionView: UICollectionView, contextMenuConfigurationForItemAt indexPath: IndexPath, point: CGPoint) -> UIContextMenuConfiguration? {
+        
         let config = UIContextMenuConfiguration(
         identifier: nil,
-        previewProvider: nil) { _ in
+        previewProvider: nil) { [ weak self] _ in
             let downloadAction = UIAction(title: "Download", subtitle: nil , image: nil ,identifier: nil, discoverabilityTitle: nil, state: .off) { _ in
-                print("Downloaad tapped")
+                self?.downloadTitleAt(indexPath: indexPath)
             }
             return UIMenu(title: "", image: nil, identifier: nil, options: .displayInline, children: [downloadAction])
             }
